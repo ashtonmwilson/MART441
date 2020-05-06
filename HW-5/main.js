4f38cd8417d7c59f1c3ab6ac49656d76c134b8c1
AshtonMWilson <ashtonmwilson@gmail.com>
2020-05-06 15:35:08 -0600

    de bugging

--- HW-5/main.js
+++ HW-5/main.js
@@ -2,7 +2,7 @@
 // all code was read and re written from Mart-441 website unless otherwise noted.
 var deckTags = ["image1","image2","image3","image4","image5","image6","image7","image8","image9","image10"];
 // making a blank image variable
-var blankCardpath = "Triangles.png";
+var blankCardpath = "imgs/Triangles.png";
 // actual images
 var actualImages = new Array();
 
@@ -14,7 +14,9 @@ function printBlanks()
   for(var i = 0; i < imageTags.lenth; i++)
   {
     document.getElementById(deckTags[i]).src= blankCardpath;
+
   }
+  alert(typeor(printBlanks));
 
 }
 
@@ -24,14 +26,14 @@ function createRandomCardArray()
   var actualCardpath= ["imgs/Face.png","imgs/Pattern12.png","imgs/Pattern23.png","imgs/Pattern34.png","imgs/Pattern16.png","imgs/Pattern19.png","imgs/Pattern3.png", "imgs/Pattern15.png","imgs/Pattern20.png","imgs/FingerPrint.png"];
   //limit images to appearing twice
   var count = [0,0];
-  while(actualImages.length < 4)
+  while(actualImages.length < 10)
   {
-      // get random junberbetween 0 and total(4)
-      var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+      // get random number between 0 and total(4)
+      var randomNumber = Math.floor(Math.random() * actualCardPath.length);
       // create an if statement that says if the total number added is less than 2, add image to array
       if(count[randomNumber] < 2)
       {
-        actualImages.push(actualImagePath[randomNumber]);
+        actualImages.push(actualCardPath[randomNumber]);
         count[randomNumber] = count[randomNumber] + 1;
       }
   }
