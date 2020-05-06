da5f35515f468b2ad897d85e03256d5de3a8dd0b
AshtonMWilson <ashtonmwilson@gmail.com>
2020-05-06 14:36:40 -0600

    saving progress

--- HW-5/main.js
+++ HW-5/main.js
@@ -1,8 +1,8 @@
 // Building the deck of memory cards
 // all code was read and re written from Mart-441 website unless otherwise noted.
-var deckTags = ["image1","image2","image3","image4"];
+var deckTags = ["image1","image2","image3","image4","image5","image6","image7","image8","image9","image10"];
 // making a blank image variable
-var blankCardpath = "filler.jpg";
+var blankCardpath = "Triangles.png";
 // actual images
 var actualImages = new Array();
 
@@ -21,7 +21,7 @@ function printBlanks()
 function createRandomCardArray()
 {
   //Building the card bank(array)
-  var actualCardpath= ["imgs/Face.png","imgs/Pattern12.png","imgs/Pattern23.png","imgs/Pattern34.png","imgs/Pattern16.png","imgs/Pattern19.png","imgs/Pattern3.png", "imgs/Pattern15.png","imgs/Pattern20.png","imgs/fingerprint.png"];
+  var actualCardpath= ["imgs/Face.png","imgs/Pattern12.png","imgs/Pattern23.png","imgs/Pattern34.png","imgs/Pattern16.png","imgs/Pattern19.png","imgs/Pattern3.png", "imgs/Pattern15.png","imgs/Pattern20.png","imgs/FingerPrint.png"];
   //limit images to appearing twice
   var count = [0,0];
   while(actualImages.length < 4)
