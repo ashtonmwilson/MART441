b857efc97d28c0cc8e97acae5b56668077e2ee83
AshtonMWilson <ashtonmwilson@gmail.com>
2020-05-06 11:26:13 -0600

    First hw-5

---
+++ HW-5/main.js
@@ -0,0 +1,44 @@
+// Building the deck of memory cards
+// all code was read and re written from Mart-441 website unless otherwise noted.
+var deckTags = ["image1","image2","image3","image4"];
+// making a blank image variable
+var blankCardpath = "filler.jpg";
+// actual images
+var actualImages = new Array();
+
+function printBlanks()
+{
+  // call on random
+  createRandomCardArray();
+  // Loop
+  for(var i = 0; i < imageTags[i]).src= blankCardpath;
+  {
+    document.getElementById(deckTags[i]).src= blankCardpath;
+  }
+
+}
+
+function createRandomCardArray()
+{
+  //Building the card bank(array)
+  var actualCardpath= ["imgs/Face.png","imgs/Pattern12.png","imgs/Pattern23.png","imgs/Pattern34.png"]
+  //limit images to appearing twice
+  var count = [0,0];
+  while(actualImages.length < 4)
+  {
+      // get random junberbetween 0 and total(4)
+      var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+      // create an if statement that says if the total number added is less than 2, add image to array
+      if(count[randomNumber] < 2)
+      {
+        actualImages.push(actualImagePath[randomNumber] <2);
+        if(count[randomNumber] = count[randomNumber] + 1;
+      }
+  }
+}
+}
+function flipCard(number)
+{
+  document.getElementById(imageTags[number]).src= actualImages[number];
+  //uses number pressed to change images....
+}
--- HW-5/readme.md
+++ HW-5/readme.md
@@ -1,8 +1,9 @@
 # Thoughts from this week:
-Moving right along, excited to learn new javascript tools.
+Cool concept, I was glad to have the debugging order of operations lesson.
 
 ## Some challenges I faced:
-* i still struggle with the little things, spelling, and remembering tags.
-* Realising as a visual learner, a diagram with both the .js and the Html next to eachother, with lines drasn back and fourth connecting the paths would really help, maybe I'll make some this sumer.
-* I am not sure why sometimes when a selection is made, the formatting stays... and other times it does not, even when the code is the same.
-* Really appreciated the walk through, Having examples on the site that are built inside html rather then between the three documents is hard for me. I appreciate the ability to pause the video, and then build my own version.
+* time is running out
+* working from home only
+* my wife is around all the time
+* thought corona would make things easier.
+*It seems like the next stip will be debugging
