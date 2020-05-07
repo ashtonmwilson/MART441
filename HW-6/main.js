8676484491de3df26fe298f4a9e21bec3b016bed
AshtonMWilson <ashtonmwilson@gmail.com>
2020-05-07 14:50:24 -0600

    Fixed actualImage.length numbers

--- HW-6/main.js
+++ HW-6/main.js
@@ -38,13 +38,13 @@ function createRandomImageArray()
     // create another array to make sure the images only get added twice
     var count = [0,0,0,0,0,0,0,0];
     // create a while statement to check to see if our actual image array is full
-    while(actualImages.length < 16)
+    while(actualImages.length < 15)
     {
         // get a random number between 0 and the number total number of images that we can choose from
         var randomNumber = Math.floor(Math.random() * actualImagePath.length);
           // create an if statement that says if the total number added is less than 2, then
         // add the image to the actual image array
-        if(count[randomNumber] < 4)
+        if(count[randomNumber] < 2)
         {
             actualImages.push(actualImagePath[randomNumber]);
             // then add one to the array that makes sure only two images can be added
